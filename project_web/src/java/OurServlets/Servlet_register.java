1a8a2add3ec441de1a39d32079ff5e7a981fa0d2
Erick Efraín <vargas.erick030997@gmail.com>
2018-05-13 01:17:16 -0500

    Something
    something

--- project_web/src/java/OurServlets/Servlet_register.java
+++ project_web/src/java/OurServlets/Servlet_register.java
@@ -7,22 +7,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class Servlet_register extends HttpServlet {
-
+    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
         response.setContentType("text/html;charset=UTF-8");
         
-        String name = request.getParameter("nombre");
-        String last_name = request.getParameter("apellido");
+        String name = request.getParameter("name");
+        String last_name = request.getParameter("last_name");
         String email = request.getParameter("email");
-        String user_name = request.getParameter("usuario");
-        String password = request.getParameter("contrasenia");
-        //String type = request.getParameter("tipo");
+        String user_name = request.getParameter("user");
+        String password = request.getParameter("password");
 
         System.out.println("Estoy en el servlet");
-        //System.out.println(type);
+        
         
     }
 }
