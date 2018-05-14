812c945edd995fa7769a471d1af5cec4b04d8762
JesusDiaz08 <jesus_kdm@hotmail.com>
2018-05-14 18:39:38 -0500

    Lunes

--- project_web/src/java/OurServlets/Servlet_register.java
+++ project_web/src/java/OurServlets/Servlet_register.java
@@ -3,6 +3,8 @@ package OurServlets;
 import static Utilities.OurXML.*;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,53 +13,90 @@ import javax.servlet.http.HttpServletResponse;
 import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.input.SAXBuilder;
+import org.jdom.output.Format;
+import org.jdom.output.XMLOutputter;
 
 public class Servlet_register extends HttpServlet {
     
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
+        String path = request.getRealPath("\\xml_code");
         Document document = null;
         Element rootElement = null;
-        File file = new File("");
-        
+        path += "\\storage.xml";
+        System.out.println(" ->-> "+path);
+        File file = new File(path);
+        SAXBuilder saxBuilder = new SAXBuilder();
         response.setContentType("text/html;charset=UTF-8");
-         
+
         String name = request.getParameter("name");
         String last_name = request.getParameter("last_name");
         String email = request.getParameter("email");
         String user_name = request.getParameter("user");
         String password = request.getParameter("password");
-
+<<<<<<< HEAD
         
-        try{
+=======
+>>>>>>> 9eaf95ba712e9ab5fdb798b7986ca54ceea94c0b
+        
+        /** BEGIN XML treat**/
+        try {
             //The file exists
-            if(file.exists()){
-                FileInputStream fileInputStream = new FileInputStream(file);
-                SAXBuilder saxBuilder = new SAXBuilder();
-                document = saxBuilder.build(fileInputStream);
+            if (file.exists()) {
+                System.out.println("File exists ");
+                //FileInputStream fileInputStream = new FileInputStream(file);
+                //SAXBuilder saxBuilder = new SAXBuilder();
+                document = saxBuilder.build(file);
                 rootElement = document.getRootElement();
-                fileInputStream.close();
-            } 
-            //The file doesn't exists
+                //fileInputStream.close();
+            } //The file doesn't exists
             else {
+                System.out.println("File doesn't exists");
                 document = new Document();
                 rootElement = new Element(ROOT);
             }
-            
+            System.out.println("adding a user data");
             Element child = new Element(USER);
-            //child.addContent(new Element);
-            
-        } catch(Exception e){
-            System.err.println("An exception has occurred searching the XML document");
+            child.addContent(new Element(NAME).setText(name));
+            child.addContent(new Element(LAST_NAME).setText(last_name));
+            child.addContent(new Element(USER_NAME).setText(user_name));
+            child.addContent(new Element(PASSWORD).setText(password));
+            child.addContent(new Element(EMAIL).setText(email));
+            child.addContent(new Element(PROJECTS).setText("algo"));
+            rootElement.addContent(child);
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            XMLOutputter xmlOutputter = new XMLOutputter(Format.getPrettyFormat());
+            xmlOutputter.output(document, fileOutputStream);
+            xmlOutputter.output(document, System.out);
+            fileOutputStream.flush();
+            fileOutputStream.close();
+
+        } catch(IOException e){ 
+            System.err.println("An IOException has occurred " + e);
+        } catch (Exception e) {
+            System.err.println("An exception has occurred searching the XML document " + e);
         }
+        /** END XML treat **/
     }
-    
-            
-        private Element setUserDatas(){
-            Element element = new Element(USER);
-            
-            return element;
-        }
+
+    /**
+     * This method adds an user in the XML code using the datas captured in the form register
+     * Parameters: Five strings that are the datas of an user
+     * Return: An element object that contains infomation about an user
+     **/
+    private Element setUserDatas(Element element, String name, String last_name, String user_name, 
+            String password, String email) throws Exception{
+        System.out.println("setUserDatas name = [" + name + "] last_name = [" + last_name + "] user_name = [" + user_name + "]"
+                + " password = [" + password +"] = email = [" + email + "]");
+        //Element element = new Element(USER);    
+        element.addContent(new Element(NAME).setText(name));
+        element.addContent(new Element(LAST_NAME).setText(last_name));
+        element.addContent(new Element(USER_NAME).setText(user_name));
+        element.addContent(new Element(PASSWORD).setText(password));
+        element.addContent(new Element(EMAIL).setText(email));
+        element.addContent(new Element(PROJECTS).setText("algo"));
+        return element;
+    }
 }
--- project_web/src/java/Utilities/OurXML.java
+++ project_web/src/java/Utilities/OurXML.java
@@ -4,12 +4,12 @@ package Utilities;
 public class OurXML {
     
     public static final String ROOT = "students";
-    public static final String user = "user";
-    public static final String name = "name";
-    public static final String last_name = "last_name";
-    public static final String user_name = "user_name";
-    public static final String password = "password";
-    public static final String email = "email";
-    public static final String projects = "projects";
-    public static final String project = "project";
+    public static final String USER = "user";
+    public static final String NAME = "name";
+    public static final String LAST_NAME = "last_name";
+    public static final String USER_NAME = "user_name";
+    public static final String PASSWORD = "password";
+    public static final String EMAIL = "email";
+    public static final String PROJECTS = "projects";
+    public static final String PROJECT = "project";
 }
