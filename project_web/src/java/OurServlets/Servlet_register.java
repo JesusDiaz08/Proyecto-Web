800c8bebbc373d48f4ad0f3d00441909922c3975
Erick Efraín <vargas.erick030997@gmail.com>
2018-05-13 23:54:34 -0500

    JDOM Register
    I've added a little bit code to insert a new user in out XML document, also I've created a method called setUserDatas in Servlet_register.java

--- project_web/src/java/OurServlets/Servlet_register.java
+++ project_web/src/java/OurServlets/Servlet_register.java
@@ -1,10 +1,16 @@
 package OurServlets;
 
+import static Utilities.OurXML.*;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.input.SAXBuilder;
 
 public class Servlet_register extends HttpServlet {
     
@@ -12,16 +18,46 @@ public class Servlet_register extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
-        response.setContentType("text/html;charset=UTF-8");
+        Document document = null;
+        Element rootElement = null;
+        File file = new File("");
         
+        response.setContentType("text/html;charset=UTF-8");
+         
         String name = request.getParameter("name");
         String last_name = request.getParameter("last_name");
         String email = request.getParameter("email");
         String user_name = request.getParameter("user");
         String password = request.getParameter("password");
 
-        System.out.println("Estoy en el servlet");
         
-        
+        try{
+            //The file exists
+            if(file.exists()){
+                FileInputStream fileInputStream = new FileInputStream(file);
+                SAXBuilder saxBuilder = new SAXBuilder();
+                document = saxBuilder.build(fileInputStream);
+                rootElement = document.getRootElement();
+                fileInputStream.close();
+            } 
+            //The file doesn't exists
+            else {
+                document = new Document();
+                rootElement = new Element(ROOT);
+            }
+            
+            Element child = new Element(USER);
+            //child.addContent(new Element);
+            
+        } catch(Exception e){
+            System.err.println("An exception has occurred searching the XML document");
+        }
     }
+    
+            
+        private Element setUserDatas(){
+            Element element = new Element(USER);
+            
+            return element;
+        }
 }
---
+++ project_web/src/java/Utilities/OurXML.java
@@ -0,0 +1,15 @@
+package Utilities;
+
+
+public class OurXML {
+    
+    public static final String ROOT = "students";
+    public static final String user = "user";
+    public static final String name = "name";
+    public static final String last_name = "last_name";
+    public static final String user_name = "user_name";
+    public static final String password = "password";
+    public static final String email = "email";
+    public static final String projects = "projects";
+    public static final String project = "project";
+}
